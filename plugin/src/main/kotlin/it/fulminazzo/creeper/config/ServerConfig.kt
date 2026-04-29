6ce119f31effef0f965e20d682d9028ebafd0b01
fulminazzo <fulminazzo21@gmail.com>
2026-04-30 01:26:39 +0200

    Created abstract ServerConfig with associated builder and methods for setting flags

---
+++ plugin/src/main/kotlin/it/fulminazzo/creeper/config/ServerConfig.kt
@@ -0,0 +1,53 @@
+package it.fulminazzo.creeper.config
+
+/**
+ * General server configuration.
+ *
+ * @property eula if `false` the server will not start
+ * @property port the port the server should run on
+ * @property players the maximum number of players allowed
+ * @property whitelist if `true` only players on the whitelist can join
+ * @property flags the JVM flags to use
+ * @constructor Creates a new Server config
+ */
+sealed class ServerConfig(
+    val eula: Boolean,
+    val port: Int,
+    val players: Int,
+    val whitelist: Boolean,
+    val flags: String
+)
+
+/**
+ * Builder for [ServerConfig].
+ *
+ * @constructor Creates a new Server config builder
+ */
+sealed class ServerConfigBuilder {
+    var eula: Boolean = false
+    var port: Int = 25565
+    var players: Int = 20
+    var whitelist: Boolean = false
+    protected val flags: JvmFlagsBuilder = JvmFlagsBuilder()
+
+    var minimumRam: MemorySize by flags::minimumRam
+    var maximumRam: MemorySize by flags::maximumRam
+
+    /**
+     * Builds the server config.
+     *
+     * @return the server config
+     */
+    abstract fun build(): ServerConfig
+
+    /**
+     * Applies the given configuration to the JVM flags builder.
+     *
+     * @param configuration the configuration
+     * @receiver the JVM flags builder
+     */
+    fun flags(configuration: JvmFlagsBuilder.() -> Unit) {
+        flags.apply(configuration)
+    }
+
+}
