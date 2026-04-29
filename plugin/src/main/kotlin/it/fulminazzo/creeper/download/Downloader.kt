d6d0d7b8a24d7bcf2f73994bb5a1ea1d204cf80c
fulminazzo <fulminazzo21@gmail.com>
2026-04-29 18:03:03 +0200

    Created HttpDownloader class with internal client and logic

--- plugin/src/main/kotlin/it/fulminazzo/creeper/download/Downloader.kt
+++ plugin/src/main/kotlin/it/fulminazzo/creeper/download/Downloader.kt
@@ -1,6 +1,13 @@
 package it.fulminazzo.creeper.download
 
+import it.fulminazzo.creeper.ProjectInfo
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
 import java.nio.file.Path
+import kotlin.time.Duration.Companion.seconds
+import kotlin.time.toJavaDuration
 
 /**
  * Function to download resources from the web.
@@ -14,5 +21,44 @@ fun interface Downloader {
      * @param destination the destination path where it will be stored
      */
     fun download(resource: String, destination: Path)
+
+    companion object {
+
+        /**
+         * Creates a new [Downloader] that uses the HTTP protocol.
+         *
+         * @return the downloader
+         */
+        fun http(): Downloader = HttpDownloader()
+
+    }
+
+}
+
+/**
+ * Base implementation of [Downloader] that uses the HTTP protocol.
+ *
+ * @constructor Create an empty Http downloader
+ */
+private class HttpDownloader : Downloader {
+    private val client = HttpClient.newBuilder()
+        .version(HttpClient.Version.HTTP_2)
+        .connectTimeout(30.seconds.toJavaDuration())
+        .followRedirects(HttpClient.Redirect.NORMAL)
+        .build()
+
+    override fun download(resource: String, destination: Path) {
+        val request = HttpRequest.newBuilder()
+            .header("User-Agent", USER_AGENT)
+            .uri(URI.create(resource))
+            .build()
+
+        client.send(request, HttpResponse.BodyHandlers.ofFile(destination))
+    }
+
+    companion object {
+        const val USER_AGENT = "${ProjectInfo.NAME}/${ProjectInfo.VERSION}"
+
+    }
 
 }
