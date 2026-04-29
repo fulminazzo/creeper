31f54a9bc6268f25d94631fb78a44cb9a740b301
fulminazzo <fulminazzo21@gmail.com>
2026-04-29 20:41:37 +0200

    Added getBuild method to retrieve specific build information

--- plugin/src/main/kotlin/it/fulminazzo/creeper/provider/MCJarsApiProvider.kt
+++ plugin/src/main/kotlin/it/fulminazzo/creeper/provider/MCJarsApiProvider.kt
@@ -1,6 +1,7 @@
 package it.fulminazzo.creeper.provider
 
 import it.fulminazzo.creeper.ProjectInfo
+import it.fulminazzo.creeper.ServerType
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 import java.io.Serial
@@ -8,6 +9,7 @@ import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
+import java.util.*
 import kotlin.time.Duration.Companion.seconds
 import kotlin.time.toJavaDuration
 
@@ -20,6 +22,40 @@ class MCJarsApiProvider {
         .connectTimeout(30.seconds.toJavaDuration())
         .followRedirects(HttpClient.Redirect.NORMAL)
         .build()
+
+    /**
+     * Gets the requested build information.
+     *
+     * @param type the platform
+     * @param version the version of the build
+     * @return the build information (or `null` if the build was not found)
+     */
+    fun getBuild(type: ServerType.MinecraftType, version: String): BuildResponse? {
+        @Serializable
+        data class Installation(val url: String, val size: Long)
+
+        @Serializable
+        data class BuildData(val uuid: String, val installations: List<Installation>)
+
+        @Serializable
+        data class Build(val data: List<BuildData>)
+
+        @Serializable
+        data class RawBuildResponse(val builds: Build)
+
+        val url = getBuildUrl(type, version)
+        return getFromApi(url)
+            ?.let { Json.decodeFromString<RawBuildResponse>(it) }
+            ?.let {
+                val firstBuildData = it.builds.data.first()
+                val firstInstallation = firstBuildData.installations.first()
+                BuildResponse(
+                    UUID.fromString(firstBuildData.uuid),
+                    firstInstallation.size,
+                    firstInstallation.url
+                )
+            }
+    }
 
     /**
      * Executes a GET request to the given [url] and returns the response body.
@@ -45,6 +81,17 @@ class MCJarsApiProvider {
 
     companion object {
         private const val API_URL = "https://mcjars.app/api/v3/"
+        private const val BUILDS_URL = $$"builds/types/%1$s/versions/%2$s"
+
+        /**
+         * Gets the URL to get the build information for the given [type] and [version].
+         *
+         * @param type the platform
+         * @param version the version of the build
+         * @return the URL
+         */
+        private fun getBuildUrl(type: ServerType.MinecraftType, version: String): String =
+            BUILDS_URL.format(type.name.uppercase(), version)
 
     }
 
@@ -78,3 +125,13 @@ class MCJarsApiProvider {
     private data class ErrorResponse(val errors: List<String>)
 
 }
+
+/**
+ * The build response when querying the API.
+ *
+ * @property uuid the UUID of the build
+ * @property size the size of the build
+ * @property url the URL to download the build
+ * @constructor Create a new Build response
+ */
+data class BuildResponse(val uuid: UUID, val size: Long, val url: String)
