043fb488af1e3218a6631dce61529d9900eecde5
fulminazzo <fulminazzo21@gmail.com>
2026-05-02 02:25:30 +0200

    Updated Modrinth plugin provider support with project name and slug

--- plugin/src/main/kotlin/it/fulminazzo/creeper/provider/plugin/ModrinthPluginProvider.kt
+++ plugin/src/main/kotlin/it/fulminazzo/creeper/provider/plugin/ModrinthPluginProvider.kt
@@ -1,7 +1,5 @@
 package it.fulminazzo.creeper.provider.plugin
 
-import com.fasterxml.jackson.annotation.JsonCreator
-import com.fasterxml.jackson.annotation.JsonIgnore
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 import it.fulminazzo.creeper.CreeperPlugin
@@ -11,6 +9,7 @@ import it.fulminazzo.creeper.download.CachedDownloader
 import it.fulminazzo.creeper.util.HttpUtils
 import it.fulminazzo.creeper.util.sha256
 import it.fulminazzo.creeper.util.urlEncode
+import org.apache.tools.ant.Project
 import org.slf4j.Logger
 import tools.jackson.module.kotlin.jacksonObjectMapper
 import tools.jackson.module.kotlin.readValue
@@ -52,7 +51,7 @@ class ModrinthPluginProvider internal constructor(
      */
     internal fun fetchVersionFileMetadata(request: ModrinthPluginRequest): CompletableFuture<VersionFile?> =
         cache[request.toHashString()]?.let { CompletableFuture.completedFuture(it) }
-            ?: HttpUtils.getApi(getVersionsUrl(request.projectName), executor).thenApply { r ->
+            ?: fetchVersionsMetadata(request.projectName).thenApply { r ->
                 r?.let { raw -> JSON_MAPPER.readValue<List<Version>>(raw) }
                     ?.firstOrNull { it.versionNumber == request.version || it.name == request.version }
                     ?.files
@@ -63,6 +62,25 @@ class ModrinthPluginProvider internal constructor(
                         versionFile
                     }
             }
+
+    /**
+     * Attempts to get the raw versions information from the API.
+     * If it fails, it attempts to fetch the slug of the given project name.
+     * Then, it re-tries getting the raw versions with the new slug.
+     *
+     * @param projectName the project name
+     * @return the versions or `null` if not found
+     */
+    private fun fetchVersionsMetadata(projectName: String): CompletableFuture<String?> {
+        return HttpUtils.getApi(getVersionsUrl(projectName), executor).thenCompose { raw ->
+            if (raw != null) CompletableFuture.completedFuture(raw)
+            else HttpUtils.getApi(getSearchUrl(projectName), executor).thenApply { r ->
+                r?.let { raw -> JSON_MAPPER.readValue<SearchResponse>(raw) }?.hits?.first()?.slug
+            }.thenCompose { slug ->
+                slug?.let { HttpUtils.getApi(getVersionsUrl(slug), executor) }
+            }
+        }
+    }
 
     override fun handleRequest(request: ModrinthPluginRequest): CompletableFuture<Path> {
         logger.info("Fetching Modrinth release information for ${request.projectName} (version = ${request.version}, filename = ${request.name})")
@@ -90,6 +108,17 @@ class ModrinthPluginProvider internal constructor(
         private fun getVersionsUrl(projectName: String): String =
             "https://api.modrinth.com/v2/project/${projectName.urlEncode()}/version"
 
+        /**
+         * Gets the URL to search for a project in the Modrinth API.
+         *
+         * @param projectName the name of the project to search for
+         * @return the URL
+         */
+        private fun getSearchUrl(projectName: String): String {
+            val search = "\"title:${projectName}\"".urlEncode()
+            return "https://api.modrinth.com/v2/search?facets=[[$search]]"
+        }
+
     }
 
     @JsonIgnoreProperties(ignoreUnknown = true)
@@ -114,12 +143,18 @@ class ModrinthPluginProvider internal constructor(
     @JsonIgnoreProperties(ignoreUnknown = true)
     private data class Hashes(val sha512: String)
 
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    private data class SearchResponse(val hits: List<SearchHit>)
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    private data class SearchHit(val slug: String)
+
 }
 
 /**
  * Plugin request for [ModrinthPluginProvider].
  *
- * @property projectName the name of the project in Modrinth
+ * @property projectName the name of the project in Modrinth (either title or slug)
  * @property version the name or number of the version
  * @property name the name of the plugin file
  * @constructor Creates a new Modrinth plugin request
