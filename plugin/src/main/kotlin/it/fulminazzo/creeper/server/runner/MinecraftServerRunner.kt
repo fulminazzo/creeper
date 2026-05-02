43ce6f9c7cec8c97baac0025b23e00ec3d4df1cc
fulminazzo <fulminazzo21@gmail.com>
2026-05-02 20:24:23 +0200

    Created MinecraftServerRunner with isBootCompleteLine method

---
+++ plugin/src/main/kotlin/it/fulminazzo/creeper/server/runner/MinecraftServerRunner.kt
@@ -0,0 +1,41 @@
+package it.fulminazzo.creeper.server.runner
+
+import it.fulminazzo.creeper.server.ServerType
+import it.fulminazzo.creeper.server.spec.MinecraftServerSpec
+import it.fulminazzo.creeper.server.spec.settings.MinecraftServerSettings
+import org.slf4j.Logger
+import java.nio.file.Path
+import java.util.concurrent.Executor
+
+/**
+ * Special implementation of [MinecraftServerRunner] for Minecraft servers.
+ *
+ * @constructor Creates a new Minecraft server installer
+ *
+ * @param specification the specification of the server to install
+ * @param logger the logger to use for logging
+ * @param executor the executor to use for asynchronous operations
+ * @param directory the directory where the server files are stored
+ */
+class MinecraftServerRunner(
+    specification: MinecraftServerSpec,
+    logger: Logger,
+    executor: Executor,
+    directory: Path
+) : ServerRunner<ServerType.MinecraftType, MinecraftServerSettings, MinecraftServerSpec>(
+    specification,
+    logger,
+    executor,
+    directory
+) {
+
+    override fun isBootCompleteLine(line: String): Boolean =
+        COMPLETE_LINE_PATTERN.matchEntire(line)?.let { true } ?: false
+
+    private companion object {
+        val COMPLETE_LINE_PATTERN =
+            "^[0-9]{2}:[0-9]{2}:[0-9]{2} INFO]: Done ([0-9]+.[0-9]+s)! For help, type \"help\"$".toRegex()
+
+    }
+
+}
