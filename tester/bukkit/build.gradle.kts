27eefe4ab575f39267637a94d8dd9e63a559b3fa
fulminazzo <fulminazzo21@gmail.com>
2026-05-03 18:16:16 +0200

    Added slf4j-jdk dependency

--- tester/bukkit/build.gradle.kts
+++ tester/bukkit/build.gradle.kts
@@ -3,6 +3,8 @@ val integrationTestImplementation by configurations.getting {}
 dependencies {
     compileOnly(libs.spigot)
 
+    implementation(libs.slf4j.jdk)
+
     testImplementation(libs.spigot)
     integrationTestImplementation(libs.spigot)
 }
