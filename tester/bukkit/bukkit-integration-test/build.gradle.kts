a90d522632b894d9ba265c13b3596775e202d8fa
fulminazzo <fulminazzo21@gmail.com>
2026-05-03 20:27:03 +0200

    Added relocations of packages in bukkit integration test to simulate actual functioning

--- tester/bukkit/bukkit-integration-test/build.gradle.kts
+++ tester/bukkit/bukkit-integration-test/build.gradle.kts
@@ -8,3 +8,29 @@ dependencies {
     implementation(project(":bukkit"))
     implementation(project(":integration-test"))
 }
+
+tasks.shadowJar {
+    val basePackage = "${rootProject.group}.${rootProject.name}.testlibs"
+    mapOf(
+        "org.apache.groovy" to "apache.groovy",
+        "org.codehaus.groovy" to "codehaus.groovy",
+        "groovy" to "groovy",
+        "groovyjarjarantlr4" to "groovyjarjarantlr4",
+        "groovyjarjarasm" to "groovyjarjarasm",
+        "groovyjarjarpicocli" to "groovyjarjarpicocli",
+        "spock" to "spock",
+        "org.spockframework" to "spockframework",
+        "munit" to "munit",
+        "io.leangen.geantyref" to "geantyref",
+        "javax" to "javax",
+        "org.slf4j" to "slf4j"
+    ).forEach { (from, to) -> relocate(from, "$basePackage.$to") }
+
+    dependencies {
+        exclude("dsld/**", "sbt/**", "org/scalatools/**")
+
+        listOf("groovy-release-info.properties", "dgminfo", "resources/**").forEach { exclude("META-INF/$it") }
+    }
+
+    mergeServiceFiles()
+}
