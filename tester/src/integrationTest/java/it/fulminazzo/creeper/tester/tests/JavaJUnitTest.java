6a49bee164d9e89bbf89dbe0436d79385dc03e48
fulminazzo <fulminazzo21@gmail.com>
2026-05-03 15:42:15 +0200

    Added JavaJUnit tests for TestsRunner integration tests

---
+++ tester/src/integrationTest/java/it/fulminazzo/creeper/tester/tests/JavaJUnitTest.java
@@ -0,0 +1,14 @@
+package it.fulminazzo.creeper.tester.tests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class JavaJUnitTest {
+
+    @Test
+    void test() {
+        assertTrue(true);
+    }
+
+}
