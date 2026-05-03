2d54d64945cd145af9c9bbef2ca36edf494be02f
fulminazzo <fulminazzo21@gmail.com>
2026-05-04 00:47:42 +0200

    Created ConfigAction

---
+++ plugin/src/main/kotlin/it/fulminazzo/creeper/task/server/install/ConfigAction.kt
@@ -0,0 +1,56 @@
+package it.fulminazzo.creeper.task.server.install
+
+import it.fulminazzo.creeper.server.spec.ServerSpec
+import it.fulminazzo.creeper.server.spec.settings.MinecraftServerSettings
+
+/**
+ * Represents an action to be performed on the server configuration file.
+ */
+sealed class ConfigAction {
+
+    /**
+     * Applies this action to the given configuration.
+     *
+     * @param configuration the configuration
+     * @param specification the specification that requested the configuration
+     */
+    abstract fun apply(configuration: MutableMap<String, Any>, specification: ServerSpec<*, *>)
+
+    /**
+     * Applies all the specified settings in a [MinecraftServerSettings] to the `server.properties` file.
+     */
+    data object ServerProperties : ConfigAction() {
+
+        override fun apply(configuration: MutableMap<String, Any>, specification: ServerSpec<*, *>) {
+            val settings = specification.settings
+            if (settings is MinecraftServerSettings) {
+                configuration["hardcore"] = settings.hardcore
+                configuration["server-port"] = settings.port
+                configuration["max-players"] = settings.players
+                configuration["difficulty"] = settings.difficulty.name.lowercase()
+                configuration["gamemode"] = settings.gamemode.name.lowercase()
+                configuration["generate-structures"] = settings.generateStructures
+                configuration["online-mode"] = settings.onlineMode
+                configuration["spawn-protection"] = settings.spawnProtection
+                configuration["view-distance"] = settings.viewDistance
+                configuration["simulation-distance"] = settings.simulationDistance
+                configuration["white-list"] = settings.whitelist
+
+                configuration["allow-nether"] = false
+            }
+        }
+
+    }
+
+    /**
+     * Applies optimization settings to the `bukkit.yml` file.
+     */
+    data object BukkitConfig : ConfigAction() {
+
+        override fun apply(configuration: MutableMap<String, Any>, specification: ServerSpec<*, *>) {
+            configuration["settings.allow-end"] = false
+        }
+
+    }
+
+}
