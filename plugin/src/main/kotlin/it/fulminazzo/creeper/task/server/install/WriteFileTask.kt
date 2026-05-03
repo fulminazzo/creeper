74ff10ce5b0216faa0d75422b470f3c87c05bd8d
fulminazzo <fulminazzo21@gmail.com>
2026-05-04 01:07:30 +0200

    Created WriteFileTask

---
+++ plugin/src/main/kotlin/it/fulminazzo/creeper/task/server/install/WriteFileTask.kt
@@ -0,0 +1,41 @@
+package it.fulminazzo.creeper.task.server.install
+
+import it.fulminazzo.creeper.PlayerResolver
+import it.fulminazzo.creeper.server.spec.ServerSpec
+import org.gradle.api.DefaultTask
+import org.gradle.api.file.RegularFileProperty
+import org.gradle.api.provider.Property
+import org.gradle.api.services.ServiceReference
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.Internal
+import org.gradle.api.tasks.OutputFile
+import org.gradle.api.tasks.TaskAction
+import kotlin.io.path.name
+
+/**
+ * Task to write a general server file.
+ *
+ * @constructor Creates a new Write file task
+ */
+abstract class WriteFileTask : DefaultTask() {
+
+    @get:ServiceReference("playerResolver")
+    abstract val playerResolver: PlayerResolver
+
+    @get:Internal
+    abstract val action: Property<FileAction>
+
+    @get:Input
+    abstract val specification: Property<ServerSpec<*, *>>
+
+    @get:OutputFile
+    abstract val file: RegularFileProperty
+
+    @TaskAction
+    fun run() {
+        val actualFile = file.get().asFile.toPath()
+        logger.lifecycle("Generating server file: ${actualFile.name}")
+        action.get().apply(actualFile.parent, specification.get(), playerResolver)
+    }
+
+}
