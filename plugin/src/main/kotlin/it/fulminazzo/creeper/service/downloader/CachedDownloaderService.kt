d4523e55d0c9c3fa1d73a31665d47adff69f4410
fulminazzo <fulminazzo21@gmail.com>
2026-05-04 02:32:07 +0200

    Created CachedDownloaderService

---
+++ plugin/src/main/kotlin/it/fulminazzo/creeper/service/downloader/CachedDownloaderService.kt
@@ -0,0 +1,17 @@
+package it.fulminazzo.creeper.service.downloader
+
+import it.fulminazzo.creeper.download.CachedDownloader
+import it.fulminazzo.creeper.download.Downloader
+import org.gradle.api.services.BuildService
+import org.gradle.api.services.BuildServiceParameters
+
+/**
+ * Service for the global [CachedDownloader].
+ */
+abstract class CachedDownloaderService : BuildService<BuildServiceParameters.None>, AutoCloseable {
+
+    val downloader: CachedDownloader by lazy {
+        CachedDownloader.global(Downloader.http())
+    }
+
+}
