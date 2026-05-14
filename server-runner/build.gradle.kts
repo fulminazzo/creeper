4429a3e72c1ab0763f205f37fbd38757dcdc5408
fulminazzo <fulminazzo21@gmail.com>
2026-05-14 19:59:57 +0200

    Added spock framework for testing server-runner module

--- server-runner/build.gradle.kts
+++ server-runner/build.gradle.kts
@@ -1,9 +1,17 @@
 plugins {
     java
+    groovy
 }
 
 java {
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(8))
+    }
+}
+
+tasks.withType<Test>().configureEach {
+    dependencies {
+        implementation(libs.spock)
+        runtimeOnly(libs.junit.launcher)
     }
 }
---
+++ server-runner/src/test/groovy/it/fulminazzo/creeper/runner/ServerRunnerTest.groovy
@@ -0,0 +1,12 @@
+package it.fulminazzo.creeper.runner
+
+import spock.lang.Specification
+
+class ServerRunnerTest extends Specification {
+
+    def 'test that testing framework works'() {
+        expect:
+        true
+    }
+
+}
