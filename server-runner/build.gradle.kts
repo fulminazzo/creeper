8fbadc8f4fef7b3ec9fb8810907b74013c4287fc
fulminazzo <fulminazzo21@gmail.com>
2026-05-17 15:48:54 +0200

    Fixed missing main class attribute in JAR manifest

--- server-runner/build.gradle.kts
+++ server-runner/build.gradle.kts
@@ -9,6 +9,12 @@ java {
     }
 }
 
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "${group}.${rootProject.name}.runner.ServerRunner"
+    }
+}
+
 afterEvaluate {
     val functionalTestImplementation: Configuration by configurations
     val functionalTestRuntimeOnly: Configuration by configurations
