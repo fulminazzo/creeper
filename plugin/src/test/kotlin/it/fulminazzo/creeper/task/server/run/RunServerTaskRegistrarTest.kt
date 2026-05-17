46b2389e6a445ed578bf937c5836cac43a827832
fulminazzo <fulminazzo21@gmail.com>
2026-05-18 01:55:24 +0200

    Added tests for registering of AwaitBootComplete task

--- plugin/src/test/kotlin/it/fulminazzo/creeper/task/server/run/RunServerTaskRegistrarTest.kt
+++ plugin/src/test/kotlin/it/fulminazzo/creeper/task/server/run/RunServerTaskRegistrarTest.kt
@@ -1,11 +1,62 @@
 package it.fulminazzo.creeper.task.server.run
 
 import it.fulminazzo.creeper.task.server.RegistrarTestHelper
+import org.gradle.api.Task
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
+import kotlin.test.assertContains
 import kotlin.test.assertEquals
 
 class RunServerTaskRegistrarTest : RegistrarTestHelper() {
+
+    @ParameterizedTest
+    @ValueSource(booleans = [true, false])
+    fun `test that registerAwaitBootCompleteTask correctly registers task`(showWorkers: Boolean) {
+        val registrar = createRegistrar(showWorkers = showWorkers)
+        registrar.registerAwaitBootCompleteTask()
+
+        val (taskName, task) = testTaskMetadata<AwaitBootCompleteTask>(
+            "awaitBootComplete$taskBaseName",
+            showWorkers
+        )
+
+        assertEquals(
+            specification,
+            task.specification.orNull,
+            "Task $taskName should have specification $specification"
+        )
+        val statusFile = project.projectDir.toPath()
+            .resolve(SERVER_DIRECTORY)
+            .resolve(RunServerTaskRegistrar.STATUS_FILE_NAME)
+        assertEquals(
+            statusFile.toFile(),
+            task.statusFile.orNull?.asFile,
+            "Task $taskName should have statusFile pointing to $statusFile"
+        )
+        val logFile = project.projectDir.toPath()
+            .resolve(SERVER_DIRECTORY)
+            .resolve("logs/latest.log")
+        assertEquals(
+            logFile.toFile(),
+            task.logFile.orNull?.asFile,
+            "Task $taskName should have logFile pointing to $logFile"
+        )
+        assertEquals(
+            RunServerTaskRegistrar.DEFAULT_BOOT_AWAIT_TIMEOUT,
+            task.awaitTimeout.orNull,
+            "Task $taskName should have awaitTimeout ${RunServerTaskRegistrar.DEFAULT_BOOT_AWAIT_TIMEOUT} seconds"
+        )
+        val stopFile = project.projectDir.toPath()
+            .resolve(SERVER_DIRECTORY)
+            .resolve(RunServerTaskRegistrar.STOP_REQUIRED_FILE_NAME)
+        assertEquals(
+            stopFile.toFile(),
+            task.stopRequiredFile.orNull?.asFile,
+            "Task $taskName should have stopRequiredFile pointing to $stopFile"
+        )
+
+        testFinalizedByStopServerTask(registrar, task)
+    }
 
     @ParameterizedTest
     @ValueSource(booleans = [true, false])
@@ -38,6 +89,15 @@ class RunServerTaskRegistrarTest : RegistrarTestHelper() {
             statusFile.toFile(),
             task.statusFile.orNull?.asFile,
             "Task $taskName should have statusFile pointing to $statusFile"
+        )
+    }
+
+    private fun testFinalizedByStopServerTask(registrar: RunServerTaskRegistrar, task: Task) {
+        val stopServerTask = registrar.stopServerTask
+        assertContains(
+            task.finalizedBy.getDependencies(task),
+            stopServerTask,
+            "Task ${task.name} should be finalized by ${stopServerTask.name}"
         )
     }
 
