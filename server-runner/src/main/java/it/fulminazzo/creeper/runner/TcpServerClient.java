eccd10d486d24e1493f49c1681d5d42d0df24e27
fulminazzo <fulminazzo21@gmail.com>
2026-05-17 12:21:04 +0200

    Created TcpServerClient

---
+++ server-runner/src/main/java/it/fulminazzo/creeper/runner/TcpServerClient.java
@@ -0,0 +1,101 @@
+package it.fulminazzo.creeper.runner;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Handles a TCP client connection.
+ *
+ * @see TcpServer
+ */
+final class TcpServerClient extends Thread implements InputListener, Closeable {
+    private static long counter = 0;
+
+    private final @NotNull Logger log;
+
+    private final @NotNull InputProcessor inputProcessor;
+    private final @NotNull OutputEmitter outputEmitter;
+
+    private final @NotNull Socket client;
+    private final @NotNull BufferedWriter output;
+
+    /**
+     * Instantiates a new Tcp server client.
+     *
+     * @param client the client
+     */
+    private TcpServerClient(
+            final @NotNull InputProcessor inputProcessor,
+            final @NotNull OutputEmitter outputEmitter,
+            final @NotNull Socket client
+    ) throws IOException {
+        super(String.format("%s-%s", TcpServerClient.class.getSimpleName(), counter++));
+        this.log = Logger.getLogger(getName());
+
+        this.inputProcessor = inputProcessor;
+        this.outputEmitter = outputEmitter;
+
+        this.client = client;
+        this.output = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+    }
+
+    @Override
+    public void run() {
+        inputProcessor.register(this);
+        try (
+                InputStreamReader reader = new InputStreamReader(client.getInputStream());
+                BufferedReader input = new BufferedReader(reader)
+        ) {
+            String line;
+            while ((line = input.readLine()) != null) outputEmitter.emit(line);
+        } catch (IOException e) {
+            log.log(Level.SEVERE, "Error reading from process", e);
+        }
+    }
+
+    @Override
+    public void processInput(final @NotNull String input) throws IOException {
+        output.write(input);
+        output.flush();
+    }
+
+    @Override
+    public void close(final @Nullable String reason) {
+        if (reason != null)
+            try {
+                processInput(reason);
+            } catch (IOException ignored) {
+                // do not log any error
+            }
+        close();
+    }
+
+    @Override
+    public void close() {
+        interrupt();
+        inputProcessor.unregister(this);
+    }
+
+    /**
+     * Creates a new TCP server client.
+     *
+     * @param inputProcessor the input processor
+     * @param outputEmitter the output emitter
+     * @param client the actual client socket
+     * @return the new TCP server client
+     * @throws IOException if an error occurs while opening the streams of the client socket
+     */
+    public static @NotNull TcpServerClient of(
+            final @NotNull InputProcessor inputProcessor,
+            final @NotNull OutputEmitter outputEmitter,
+            final @NotNull Socket client
+    ) throws IOException {
+        return new TcpServerClient(inputProcessor, outputEmitter, client);
+    }
+
+}
