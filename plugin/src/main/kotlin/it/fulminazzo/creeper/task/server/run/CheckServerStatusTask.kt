030d55d377cee9c2b48f3fd9d05f0c7c3b4b6fc0
fulminazzo <fulminazzo21@gmail.com>
2026-05-17 17:14:18 +0200

    Created CheckServerStatus task to monitor server status

---
+++ plugin/src/main/kotlin/it/fulminazzo/creeper/task/server/run/CheckServerStatusTask.kt
@@ -0,0 +1,67 @@
+package it.fulminazzo.creeper.task.server.run
+
+import com.fasterxml.jackson.module.kotlin.readValue
+import it.fulminazzo.creeper.CreeperPlugin
+import it.fulminazzo.creeper.extension.spec.ServerSpec
+import org.gradle.api.DefaultTask
+import org.gradle.api.file.RegularFileProperty
+import org.gradle.api.provider.Property
+import org.gradle.api.tasks.Internal
+import org.gradle.api.tasks.TaskAction
+import java.io.IOException
+import java.net.Socket
+
+/**
+ * Task to check the status of the server.
+ *
+ * The status is checked against the status file.
+ * If the status file does not exist, the server is considered to be stopped.
+ * If the status file exists, the internal data is checked:
+ * - If the PID is not associated with any process, the server is considered to be stopped;
+ * - If it is not possible to connect to the TCP server, the server is considered to be stopped.
+ *
+ * If any of the above conditions are met, the status file will be deleted.
+ *
+ * This task should always be run to ensure proper functioning.
+ *
+ * @constructor Creates a new Check server status task
+ */
+abstract class CheckServerStatusTask : DefaultTask() {
+
+    @get:Internal
+    abstract val specification: Property<ServerSpec<*, *>>
+
+    @get:Internal
+    abstract val port: Property<Int>
+
+    @get:Internal
+    abstract val statusFile: RegularFileProperty
+
+    @get:Internal
+    abstract val stopRequiredFile: RegularFileProperty
+
+    @TaskAction
+    fun run() {
+        logger.lifecycle("Checking server status")
+
+        val statFile = statusFile.get().asFile
+        if (!statFile.exists()) return writeStopRequiredFile()
+
+        val data = CreeperPlugin.PROPERTIES_MAPPER.readValue<Map<String, Any>>(statFile)
+
+        val pid = data["pid"]?.toString()?.toLong() ?: return writeStopRequiredFile()
+        ProcessHandle.of(pid).orElse(null)?.takeIf { it.isAlive } ?: return writeStopRequiredFile()
+
+        val port = data["port"]?.toString()?.toInt() ?: return writeStopRequiredFile()
+        try {
+            Socket("0.0.0.0", port).close()
+        } catch (_: IOException) {
+            return writeStopRequiredFile()
+        }
+    }
+
+    private fun writeStopRequiredFile() {
+        stopRequiredFile.get().asFile.createNewFile()
+    }
+
+}
