e6811a63506d5f36c1533301bf677a6834ec258c
fulminazzo <fulminazzo21@gmail.com>
2026-05-21 00:14:59 +0200

    Updated and fixed WriteTestRunnerMetadata task to detect and write Java version

--- plugin/src/main/kotlin/it/fulminazzo/creeper/task/test/runner/WriteTestRunnerMetadataTask.kt
+++ plugin/src/main/kotlin/it/fulminazzo/creeper/task/test/runner/WriteTestRunnerMetadataTask.kt
@@ -2,15 +2,20 @@ package it.fulminazzo.creeper.task.test.runner
 
 import it.fulminazzo.creeper.ProjectInfo
 import org.gradle.api.DefaultTask
+import org.gradle.api.GradleException
 import org.gradle.api.artifacts.Configuration
-import org.gradle.api.file.ConfigurableFileCollection
 import org.gradle.api.file.RegularFileProperty
-import org.gradle.api.tasks.InputFiles
+import org.gradle.api.plugins.JavaPluginExtension
 import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
 
 /**
- * Writes the necessary data for the test runner to build.
+ * Modifies the `build.gradle` file of the `tester` module to conform to our requirements.
+ * The "requirements" are defined as the following:
+ * - Project group and name should be the same as the plugin ones;
+ * - Any `implementation`, `runtimeOnly`, `integrationTestImplementation` or `integrationTestRuntimeOnly` dependency
+ * must be delivered as `implementation` to the `tester` module, so that it can be packaged in the final JAR;
+ * - The Java compile version of the `tester` should match the one project.
  *
  * @constructor Creates a new Write test runner metadata task
  */
@@ -21,27 +26,30 @@ abstract class WriteTestRunnerMetadataTask : DefaultTask() {
 
     @TaskAction
     fun run() {
-        val targetConfigurations = setOf(
-            "implementation",
-            "runtimeOnly",
-            "integrationTestImplementation",
-            "integrationTestRuntimeOnly"
-        )
+        val javaExtension = project.extensions.findByType(JavaPluginExtension::class.java)
+            ?: throw GradleException("Java plugin not applied to the project")
 
-        val dependencies = targetConfigurations
+        val javaVersion = javaExtension.toolchain.languageVersion.orNull?.asInt()
+            ?: javaExtension.targetCompatibility.majorVersion.toInt()
+        logger.lifecycle("Detected Java version: $javaVersion")
+
+        val dependencies = REQUIRED_CONFIGURATIONS
             .mapNotNull { name -> project.configurations.findByName(name) }
             .flatMap { config -> resolveSafely(config) }
             .distinct()
 
         logger.lifecycle("Collected ${dependencies.size} dependencies")
 
-        //TODO: Java version
         val build = buildFile.get().asFile
         val lines = build.readLines().map { line ->
-            if (line.startsWith("val implementationDependencies"))
-                "val implementationDependencies = listOf(${dependencies.joinToString(", ")})"
-            else if (line.startsWith("val parentGroup")) "${ProjectInfo.GROUP}:${ProjectInfo.NAME}"
-            else if (line.startsWith("val parentVersion")) ProjectInfo.VERSION
+            if (line.startsWith("val compileJavaVersion"))
+                "val compileJavaVersion = JavaLanguageVersion.of($javaVersion)"
+            else if (line.startsWith("val gradleJavaVersion"))
+                "val gradleJavaVersion = JavaLanguageVersion.of($javaVersion)"
+            else if (line.startsWith("val implementationDependencies"))
+                "val implementationDependencies = listOf(${dependencies.joinToString(", ") { "\"$it\"" }})"
+            else if (line.startsWith("val parentGroup")) "val parentGroup = \"${ProjectInfo.GROUP}.${ProjectInfo.NAME}\""
+            else if (line.startsWith("val parentVersion")) "val parentVersion = \"${ProjectInfo.VERSION}\""
             else line
         }
         build.writeText(lines.joinToString("\n"))
@@ -55,6 +63,14 @@ abstract class WriteTestRunnerMetadataTask : DefaultTask() {
     } catch (e: Exception) {
         logger.warn("Could not resolve configuration '${config.name}': ${e.message}")
         emptyList()
+    }
+
+    private companion object {
+        private val REQUIRED_CONFIGURATIONS = setOf(
+            "implementation", "runtimeOnly",
+            "integrationTestImplementation", "integrationTestRuntimeOnly"
+        )
+
     }
 
 }
