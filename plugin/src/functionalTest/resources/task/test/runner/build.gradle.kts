bdd9ae2489268e40d2253a568811991f55219989
fulminazzo <fulminazzo21@gmail.com>
2026-05-21 01:49:41 +0200

    Fixed SetupTestRunnerRegistrar functional tests

--- plugin/src/functionalTest/resources/task/test/runner/build.gradle.kts
+++ plugin/src/functionalTest/resources/task/test/runner/build.gradle.kts
@@ -1,10 +1,16 @@
 import it.fulminazzo.creeper.service.downloader.DownloaderService
+import it.fulminazzo.creeper.task.test.runner.FetchRepositoryTask
 import it.fulminazzo.creeper.task.test.runner.SetupTestRunnerRegistrar
 
 plugins {
+    java
     id("it.fulminazzo.creeper")
 }
 
 val sharedServices = gradle.sharedServices
 sharedServices.registerIfAbsent("simpleDownloaderService", DownloaderService::class.java)
 SetupTestRunnerRegistrar.register(project, File(project.projectDir, "build").toPath())
+
+tasks.withType<FetchRepositoryTask>() {
+    branch = "plugin-test-runner"
+}
