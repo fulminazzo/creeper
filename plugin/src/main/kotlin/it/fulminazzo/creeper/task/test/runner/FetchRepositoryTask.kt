96210ec0f1d2a18d8a290ffea5fc9b5f8339784d
fulminazzo <fulminazzo21@gmail.com>
2026-05-20 23:59:44 +0200

    Fixed FetchRepository task with unzipping of downloaded repository

--- plugin/src/main/kotlin/it/fulminazzo/creeper/task/test/runner/FetchRepositoryTask.kt
+++ plugin/src/main/kotlin/it/fulminazzo/creeper/task/test/runner/FetchRepositoryTask.kt
@@ -2,17 +2,21 @@ package it.fulminazzo.creeper.task.test.runner
 
 import it.fulminazzo.creeper.ProjectInfo
 import it.fulminazzo.creeper.download.Downloader
+import it.fulminazzo.creeper.util.ZipUtils
 import org.gradle.api.DefaultTask
 import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.provider.Property
 import org.gradle.api.services.ServiceReference
 import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
+import java.io.File
 
 /**
- * Task to fetch the repository of this project.
+ * Task to fetch the latest version of this project repository from [GitHub](https://github.com/fulminazzo/creeper).
+ * The main purpose of this task is to provide access to the `tester` module to inject a specialized plugin
+ * into server platforms that will then run the requested tests.
  *
- * @constructor Creates a new Repository fetcher task
+ * @constructor Creates a new Fetch repository task
  */
 abstract class FetchRepositoryTask : DefaultTask() {
 
@@ -24,9 +28,14 @@ abstract class FetchRepositoryTask : DefaultTask() {
 
     @TaskAction
     fun run() {
-        val output = repositoryDirectory.get().asFile
-        logger.lifecycle("Fetching repository from $REPOSITORY_URL")
+        val repository = repositoryDirectory.get().asFile
+        logger.lifecycle("Fetching repository from $REPOSITORY_URL into ${repository.path}")
+        val parentFile = repository.parentFile
+        val output = File(parentFile, "tmp.zip")
         downloader.get().download(REPOSITORY_URL, output.toPath())
+        ZipUtils.unzip(output.toPath(), parentFile.toPath())
+        output.delete()
+        parentFile.resolve("${ProjectInfo.NAME}-master").renameTo(repository)
     }
 
     private companion object {
