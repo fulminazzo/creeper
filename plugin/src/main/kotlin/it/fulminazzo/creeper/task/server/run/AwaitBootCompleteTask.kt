7b6f046500f6cb5978a3673449355fd629dd82d1
fulminazzo <fulminazzo21@gmail.com>
2026-05-22 00:12:41 +0200

    Optimized AwaitBootComplete with VerifyUtils

--- plugin/src/main/kotlin/it/fulminazzo/creeper/task/server/run/AwaitBootCompleteTask.kt
+++ plugin/src/main/kotlin/it/fulminazzo/creeper/task/server/run/AwaitBootCompleteTask.kt
@@ -1,6 +1,7 @@
 package it.fulminazzo.creeper.task.server.run
 
 import it.fulminazzo.creeper.extension.spec.ServerSpec
+import it.fulminazzo.creeper.util.VerifyUtils
 import org.gradle.api.DefaultTask
 import org.gradle.api.GradleException
 import org.gradle.api.file.RegularFileProperty
@@ -9,10 +10,7 @@ import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputFile
 import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.TaskAction
-import java.util.concurrent.CountDownLatch
-import java.util.concurrent.Executors
-import java.util.concurrent.TimeUnit
-import java.util.concurrent.atomic.AtomicBoolean
+import kotlin.time.Duration.Companion.seconds
 
 /**
  * Task to verify if the server has booted successfully.
@@ -39,32 +37,22 @@ abstract class AwaitBootCompleteTask : DefaultTask() {
     @TaskAction
     fun run() {
         logger.lifecycle("Awaiting server boot completion. Timeout: ${awaitTimeout.get()} seconds")
-        val verified = AtomicBoolean(false)
-        val latch = CountDownLatch(1)
-
-        val scheduler = Executors.newSingleThreadScheduledExecutor()
 
         val spec = specification.get()
         val log = logFile.get().asFile
-        scheduler.scheduleAtFixedRate(
-            {
-                if (!verified.get())
-                    verified.set(log.exists() && log.readLines().any { spec.isBootCompleteLine(it) })
-                else latch.countDown()
-            }, 0, 1, TimeUnit.SECONDS
-        )
-
-        latch.await(awaitTimeout.get(), TimeUnit.SECONDS)
-        scheduler.shutdownNow()
-
-        if (!verified.get()) {
+        if (VerifyUtils.awaitVerified(
+                { log.exists() && log.readLines().any { spec.isBootCompleteLine(it) } },
+                awaitTimeout.get().seconds,
+            )
+        ) logger.lifecycle("Server booted successfully")
+        else {
             stopRequiredFile.get().asFile.createNewFile()
             throw GradleException(
                 "Server could not boot within ${awaitTimeout.get()} seconds. "
                         + "This could either be a problem with the process or the server might require a bigger timeout. "
                         + "Check the server log for more information: ${log.absolutePath}"
             )
-        } else logger.lifecycle("Server booted successfully")
+        }
     }
 
 }
