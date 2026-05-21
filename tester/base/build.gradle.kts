3232878eb1c40912f08b1f937f73bbf6a14b4d96
fulminazzo <fulminazzo21@gmail.com>
2026-05-21 16:54:12 +0200

    Added YAML dependency

--- tester/base/build.gradle.kts
+++ tester/base/build.gradle.kts
@@ -12,6 +12,7 @@ val integrationTestImplementation by configurations.getting {}
 dependencies {
     compileOnly(libs.slf4j)
     compileOnly(libs.gson)
+    compileOnly(libs.yaml)
 
     api(libs.junit.launcher)
     api(libs.bundles.test.engines)
