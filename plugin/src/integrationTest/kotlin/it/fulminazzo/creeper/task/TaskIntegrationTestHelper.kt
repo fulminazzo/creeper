869e08f249b8403889e14b072dbb8d2163363041
fulminazzo <fulminazzo21@gmail.com>
2026-05-21 19:07:06 +0200

    Overridden PLUGIN_REQUEST for InjectTestRunnerDependencyTask for testing purposes

--- plugin/src/integrationTest/kotlin/it/fulminazzo/creeper/task/TaskIntegrationTestHelper.kt
+++ plugin/src/integrationTest/kotlin/it/fulminazzo/creeper/task/TaskIntegrationTestHelper.kt
@@ -1,10 +1,14 @@
 package it.fulminazzo.creeper.task
 
 import io.mockk.mockk
+import it.fulminazzo.creeper.ProjectInfo
+import it.fulminazzo.creeper.provider.plugin.LocalPluginRequest
+import it.fulminazzo.creeper.task.server.install.InjectTestRunnerDependencyTask
 import org.gradle.api.Action
 import org.gradle.api.Task
 import org.gradle.api.services.BuildService
 import org.gradle.testfixtures.ProjectBuilder
+import java.io.File
 
 abstract class TaskIntegrationTestHelper {
     protected val project = ProjectBuilder.builder().build()
@@ -16,6 +20,19 @@ abstract class TaskIntegrationTestHelper {
         val service = mockk<S>()
         configurationAction.execute(service)
         return service
+    }
+
+    private companion object {
+
+        init {
+            val testRunnerJar = File("").absoluteFile.toPath().parent
+                .resolve("tester")
+                .resolve("build")
+                .resolve("libs")
+                .resolve("${ProjectInfo.NAME}Tester-${ProjectInfo.VERSION}.jar")
+            InjectTestRunnerDependencyTask.PLUGIN_REQUEST = LocalPluginRequest(testRunnerJar, true)
+        }
+
     }
 
 }
